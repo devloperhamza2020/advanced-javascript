4c75d1a3f400d29dce4ab02ae7c73b0ea77d0683
mdamirhamza <mdahshawon12@gmail.com>
2021-02-03 00:03:28 +0600

    done

---
+++ map-filter.js
@@ -0,0 +1,16 @@
+// const numbers = [3, 4, 8, 11, 13, 15];
+// const output = [];
+// for (let i = 0; i < numbers.length; i++) {
+//     const element = numbers[i];
+//     const result = element * element;
+//     output.push(result);
+// }
+// console.log(output);
+
+
+// const result = numbers.map(x => x * x);
+// console.log(result);
+
+// const bigger = numbers.filter (x => x < 5);
+// console.log(bigger);
+
