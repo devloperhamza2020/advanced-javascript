e7f9fcf455d28338b12a4e9200b0a1d8bb92e47d
mdamirhamza <mdahshawon12@gmail.com>
2021-02-02 20:08:29 +0600

    done

---
+++ null-vs-undefined.js
@@ -0,0 +1,20 @@
+let pakhi;
+console.log(pakhi);
+
+function add(num1, num2) {
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2) {
+    console.log(num1, num2);
+}
+
+const premik = { name: 'amir', phone: '01884296377'};
+console.log(premik.address);
+
+let fun = undefined;
+console.log(fun);
+
+let ages = [17, 19, 21, 28];
+console.log(ages[11]);
